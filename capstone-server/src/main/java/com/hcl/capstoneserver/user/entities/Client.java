1d5b03acfe2b89d9780ad00c9a7fa695e88bb913
Shevan <fernandowkbst@gmail.com>
2021-04-29 10:24:12 +0530

    remove extend AppUser class and add super person class to it

--- capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Client.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Client.java
@@ -6,70 +6,18 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class Client extends AppUser{
-    @Column(unique = true)
-    private int clientId;
-
-    private String name;
-    private String address;
-    private String email;
-    private String phone;
+public class Client extends Person {
     private Float interestRate;
     private int accountNumber;
 
     public Client() {
+        super();
     }
 
-    public Client(String userId, String password, int clientId, String name,
-                  String address, String email, String phone, Float interestRate, int accountNumber) {
-        super(userId, password, "CLIENT");
-        this.clientId = clientId;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.phone = phone;
+    public Client(String userId, String password, String userType, Float id, String name, String address, String email, String phone, Float interestRate, int accountNumber) {
+        super(userId, password, "CLIENT", id, name, address, email, phone);
         this.interestRate = interestRate;
         this.accountNumber = accountNumber;
-    }
-
-    public int getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(int clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public Float getInterestRate() {
--- capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Supplier.java
+++ capstone-server/src/main/java/com/hcl/capstoneserver/user/entities/Supplier.java
@@ -6,68 +6,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class Supplier extends AppUser{
-    @Column(unique = true)
-    private int supplierId;
-
-    private String name;
-    private String address;
-    private String email;
-    private String phone;
+public class Supplier extends Person {
     private Float interestRate;
 
     public Supplier() {
     }
 
-    public Supplier(String userId, String password, int supplierId,
-                    String name, String address, String email, String phone, Float interestRate) {
-        super(userId, password, "SUPPLIER");
-        this.supplierId = supplierId;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.phone = phone;
+    public Supplier(String userId, String password, String userType, Float id, String name, String address, String email, String phone, Float interestRate) {
+        super(userId, password, "SUPPLIER", id, name, address, email, phone);
         this.interestRate = interestRate;
-    }
-
-    public int getSupplierId() {
-        return supplierId;
-    }
-
-    public void setSupplierId(int supplierId) {
-        this.supplierId = supplierId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public Float getInterestRate() {
