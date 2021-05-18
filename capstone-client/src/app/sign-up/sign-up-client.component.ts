58cc60c1d142f4724c314ee6f9fce78e932c9262
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-05-18 15:43:57 +0530

    design the client registration page

--- capstone-client/src/app/sign-up/sign-up-client.component.ts
+++ capstone-client/src/app/sign-up/sign-up-client.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonalDetails } from './personal-data-form/personal-data-form.component';
+import { UserDetails } from './user-data-form/user-data-form.component';
+
+type ClientAccDetail = {
+  creditAccNumber?: number;
+  bankCode?: number;
+  supplierLimit?: number;
+  invoicePayment?: string;
+};
 
 @Component({
   selector: 'app-sign-up-client',
   templateUrl: './sign-up-client.component.html',
-  styleUrls: ['./sign-up-client.component.scss']
+  styleUrls: ['./sign-up-client.component.scss'],
 })
 export class SignUpClientComponent implements OnInit {
+  personalDetails: PersonalDetails;
+  pageNumber: number;
+  supplierAccDetail: ClientAccDetail;
+  userDetails: UserDetails;
 
-  constructor() { }
+  constructor() {
+    this.personalDetails = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobileNumber: '',
+      addressLine1: '',
+      city: '',
+      state: '',
+      province: '',
+      country: '',
+    };
+    this.pageNumber = 1;
 
-  ngOnInit(): void {
+    this.supplierAccDetail = {};
+    this.userDetails = { password: '', username: '' };
   }
 
+  ngOnInit(): void {}
+
+  submitUserData = (value: UserDetails): void => {
+    this.userDetails = value;
+    this.goToNextPage();
+  };
+  submitPersonalData = (value: PersonalDetails): void => {
+    this.personalDetails = value;
+    this.goToNextPage();
+  };
+
+  gotoPreviousPage = (): void => {
+    if (this.pageNumber > 1) {
+      this.pageNumber = this.pageNumber - 1;
+    }
+  };
+
+  goToNextPage = (): void => {
+    if (this.pageNumber < 3) {
+      this.pageNumber = this.pageNumber + 1;
+    }
+  };
 }
